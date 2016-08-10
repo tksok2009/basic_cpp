57b77810675cccc5a2222136d98f185376294cc5
toysking <tksok2009@gmail.com>
2016-08-10 23:05:26 +0900

    Initial Commit

---
+++ test_c/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_c
+//
+//  Created by toysking on 2016/08/10.
+//  Copyright © 2016年 toysking. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
