516097989b340211d3de70abf9341c62601190aa
toysking <tksok2009@gmail.com>
2016-08-11 09:26:09 +0900

    hello and cin. use include<string>

--- test_c/main.cpp
+++ test_c/main.cpp
@@ -7,9 +7,14 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    string name;
     std::cout << "Hello, World!\n";
+    std::cin >> name;
+    std::cout << name << "さんよろしく";
     return 0;
 }
